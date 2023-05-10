bf19830bdda5ea8555400b96f15bb54e8a6f9b89
Mit-kundariya <46038844+Mit-kundariya@users.noreply.github.com>
2023-05-10 17:31:18 +0530

    Add files via upload

---
+++ mobile_message.py
@@ -0,0 +1,11 @@
+# signup on fast2sms website and use below code to send bulk mobile messages 
+import requests
+url = "https://www.fast2sms.com/dev/bulkV2"
+payload = "sender_id=TXTIND&message=test&language=english&route=q&numbers=<mobile_number1>,<mobile_number2>"
+headers = {
+'authorization': "<API_KEY>",
+'Content-Type': "application/x-www-form-urlencoded",
+'Cache-Control': "no-cache",
+}
+response = requests.request("POST", url, data=payload, headers=headers)
+print(response.text, response.status_code)
