7f5c8ff70ee09526323dfb79ca7d0c748e7320b9
Mit-kundariya <46038844+Mit-kundariya@users.noreply.github.com>
2023-05-10 17:32:56 +0530

    Update mobile_message.py

--- mobile_message.py
+++ mobile_message.py
@@ -3,9 +3,9 @@ import requests
 url = "https://www.fast2sms.com/dev/bulkV2"
 payload = "sender_id=TXTIND&message=test&language=english&route=q&numbers=<mobile_number1>,<mobile_number2>"
 headers = {
-'authorization': "<API_KEY>",
-'Content-Type': "application/x-www-form-urlencoded",
-'Cache-Control': "no-cache",
+	'authorization': "<API_KEY>",
+	'Content-Type': "application/x-www-form-urlencoded",
+	'Cache-Control': "no-cache",
 }
 response = requests.request("POST", url, data=payload, headers=headers)
 print(response.text, response.status_code)
